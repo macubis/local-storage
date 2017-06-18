dfb928410acc3581221bf7cf293d60a9559f41eb
macubis <macubis@gmail.com>
2017-06-18 12:37:27 -0400

    subiendo segundo ejercicio guiado

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function saveData(){
+	var name = document.getElementById("name").value;
+	var clave = document.getElementById("clave").value;
+	sessionStorage.setItem(name,clave);
+}
+function recoverData(){
+	for(var i=0; i<sessionStorage.length; i++){
+		var name=sessionStorage.key(i);
+		var clave=sessionStorage.getItem(name);
+		document.getElementById("data").innerHTML = '<div>' + 'Nombre : ' + name+ '<br/> clave : ' + clave +'<div>';
+	}
+}
+function cleanData(){
+	document.getElementById("data").innerHTML='Limpiando vista. Los datos permanecen.';
+}
+function borrarTodo(){sessionStorage.clear(); recoverData();}
